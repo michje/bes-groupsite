833e51fa6d57e30d6ed29df980123617a163c08f
Michael Tautz <michje@gmail.com>
2011-07-05 22:22:55 +0200

    generated rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BesGroupsite::Application.config.secret_token = 'c0b30f99e4b2e16fb7f2b5078e5b3954867591827d3649eefd0b7cfe1e8719672a1850e8f244f43a96c5dd9f0242e79737744977d6981e41e0b7c14bda3cc76e'
